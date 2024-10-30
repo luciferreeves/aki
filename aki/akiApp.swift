b1e1bea7e2005fc199d668a9a064b1c194cf2563
Bobby <bobbyskhs@gmail.com>
2024-10-30 16:00:00 -0400

    Initial Commit

---
+++ aki/akiApp.swift
@@ -0,0 +1,17 @@
+//
+//  akiApp.swift
+//  aki
+//
+//  Created by Conrad Reeves on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct akiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
